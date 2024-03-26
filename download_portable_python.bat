5698f78d5998bf916ce28aa56fe25228a8388bb3
dtlnor <dtlnor@hotmail.com>
2024-03-26 12:21:21 +0900

    update to python 3.12

--- download_portable_python.bat
+++ download_portable_python.bat
@@ -2,8 +2,8 @@
 REM Make a portable python environment in ./python
 
 REM Version info
-set PYTHON_VERSION=3.11.6
-set PYTHON_VER_SHORT=311
+set PYTHON_VERSION=3.12.2
+set PYTHON_VER_SHORT=312
 
 REM Delete ./python if exist
 if exist python rmdir /s python
@@ -22,6 +22,7 @@ REM Download mmh3 and chardet
 ) > python%PYTHON_VER_SHORT%._pth
 curl -OL https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
+REM If you have the requirements installed in any path, uninstall them.
 python -m pip install -r ..\requirements.txt
 robocopy Lib\site-packages\chardet chardet /E
 copy Lib\site-packages\mmh3.cp%PYTHON_VER_SHORT%-win_amd64.pyd .
@@ -33,7 +34,7 @@ cd ..
 
 REM Remove unnecessary files
 del pythonw.exe python.cat python%PYTHON_VER_SHORT%._pth
-del python3.dll libcrypto-1_1.dll libssl-1_1.dll sqlite3.dll
+del python3.dll libcrypto-1_1.dll libssl-1_1.dll libcrypto-3.dll libssl-3.dll sqlite3.dll
 del vcruntime140.dll vcruntime140_1.dll
 del _asyncio.pyd _bz2.pyd _decimal.pyd _elementtree.pyd _hashlib.pyd
 del _lzma.pyd _msi.pyd _overlapped.pyd _queue.pyd
@@ -43,8 +44,8 @@ del pyexpat.pyd unicodedata.pyd winsound.pyd
 REM Remove unnecessary files from pythonXXX.zip
 powershell Expand-Archive -Force -Path python%PYTHON_VER_SHORT%.zip
 cd python%PYTHON_VER_SHORT%
-rmdir /s /q curses dbm distutils html http lib2to3 msilib
-rmdir /s /q pydoc_data site-packages sqlite3 urllib
+rmdir /s /q __phello__ curses dbm html http lib2to3 msilib
+rmdir /s /q pydoc_data site-packages sqlite3 tomllib urllib
 rmdir /s /q wsgiref xml xmlrpc zoneinfo
 del ast.pyc calendar.pyc doctest.pyc ftplib.pyc
 del imaplib.pyc ipaddress.pyc mailbox.pyc nntplib.pyc
